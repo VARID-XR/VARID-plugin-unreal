be58b04b27bd9d26bb952758d1d6eadb0c4db4e7
make-transition <joe@maketransition.co.uk>
2021-11-02 10:48:59 +0000

    clean up public build dependencies

--- Source/VARID/VARID.Build.cs
+++ Source/VARID/VARID.Build.cs
@@ -27,24 +27,33 @@ public class VARID : ModuleRules
 				// ... add other private include paths required here ...
 			}
 			);
-			
-		
+
+		// Where you would place the dependency would depend on whether it was needed in the Public or Private folder
+        // (most dependencies are Private since the Public folder typically contains only interfaces).
+		// try to minimize public dependencies whenever we can.
+        // One common way to eliminate indirect dependencies from modules is to use forward-declarations in their public header files and only use those types and functions internally inside private files.
+
 		PublicDependencyModuleNames.AddRange(
+
+			// Public includes/ modules will be re - exported from your module.
+			// You are basically indicating that, in order to use your module, users will also require dependencies from these other headers and modules.
+			// This generally happens if your module's public header files expose and/or use types or functions that are declared in another module.
+			// This mechanism allows users of your module to add a dependency to your module without also explicitly having to add all the other indirect dependencies.
+			// In effect, the header file and linker dependencies are passed through from your module to the user's module.
+
 			new string[]
 			{
-				"Core",
-				"RenderCore", // Needed for AddShaderSourceDirectoryMapping & ResetAllShaderSourceDirectoryMappings
-				"RHI",
-				"Projects", // Needed for IPluginManager
-				"Renderer",
-				"CoreUObject",
-				"Engine",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
          
             
 		PrivateDependencyModuleNames.AddRange(
+
+			// Private includes/ modules indicate that these are internal dependencies of your module.
+            // They will not be visible to the outside world.
+            // This generally happens if your internal, private .cpp and.h files consume types or functions that are declared in another module.
+
 			new string[]
 			{
                 "Core",
